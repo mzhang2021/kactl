a7f288d09b0467b5989c063a2e4d4b99f1d42391
mzhang2021 <maxzhang614@gmail.com>
2022-02-12 00:05:29 -0500

    removed note about bug, not a bug i was just dumb

--- content/stuff-i-added/LinkCutTree.h
+++ content/stuff-i-added/LinkCutTree.h
@@ -1,7 +1,6 @@
 /**
- * Description: Link cut tree for maintaining path or subtree aggregates while adding or removing edges. Uses 1-based indexing. I believe the lazy only works for path, not subtree.
- * KNOWN BUG: Implementation doesn't seem to pull properly for non-commutative functions?
- * See https://judge.yosupo.jp/submission/78077 vs https://judge.yosupo.jp/submission/78086, only difference is line 40.
+ * Description: Link cut tree for maintaining path or subtree aggregates while adding or removing edges. Uses 1-based indexing.
+ * I believe the lazy only works for path, not subtree.
  * Author: bicsi
  * Source: https://codeforces.com/blog/entry/75885
  * Time: O(log n) for all operations
